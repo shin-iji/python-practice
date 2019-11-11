0ff83a1fecfe58739139f78c7e7d06448428bf91
Shindanai <siradanai27@gmail.com>
2019-11-11 21:06:35 +0700

    Random Password

---
+++ Operator and Number/random_pw.py
@@ -0,0 +1,12 @@
+import random
+
+a = random.choice("0123456789qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM")
+b = random.choice("0123456789qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM")
+c = random.choice("0123456789qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM")
+d = random.choice("0123456789qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM")
+e = random.choice("0123456789qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM")
+f = random.choice("0123456789qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM")
+
+result = a + b + c + d + e + f
+
+print("Your password is: " + result)
